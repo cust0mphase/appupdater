da2467571f2dd3051c89f5a0b62e91391262e305
cust0mphase <cust0mphse@gmail.com>
2026-05-10 22:45:39 +0300

    Initial commit

---
+++ src/main/java/com/example/appupdater/AppupdaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.appupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppupdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppupdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appupdater/AppupdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppupdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
