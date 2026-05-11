8325cb5b64bc8df3159723fd98bbf0372b8b637b
cust0mphase <cust0mphse@gmail.com>
2026-05-11 17:28:13 +0300

    Init: Configure project and database connection

--- src/main/java/com/example/appupdater/AppUpdaterApplication.java
+++ src/main/java/com/example/appupdater/AppUpdaterApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppupdaterApplication {
+public class AppUpdaterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppupdaterApplication.class, args);
+		SpringApplication.run(AppUpdaterApplication.class, args);
 	}
-
 }
