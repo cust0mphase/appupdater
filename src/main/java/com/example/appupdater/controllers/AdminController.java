c74c2eaae2478f234836b1ab41fd7ed67908da7a
cust0mphase <cust0mphse@gmail.com>
2026-05-16 00:04:46 +0300

    add preauthorize role system

--- src/main/java/com/example/appupdater/controllers/AdminController.java
+++ src/main/java/com/example/appupdater/controllers/AdminController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@PreAuthorize("hasAuthority('manage_users')")
 @Tag(name = "Админ-панель", description = "CRUD для управления пользователями, ролями и правами")
 public class AdminController {
 
